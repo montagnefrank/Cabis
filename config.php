df3caca5ca49cec9f75d9a73c308e3b892b412bc
Frank Montagne <montagnefrank@gmail.com>
2016-10-27 18:48:35 -0500

    Update
    Limpieza y depuración de archivos complementarios

--- config.php
+++ config.php
@@ -7,7 +7,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="favicon.ico" type="image/x-icon" />
-        <!-- FIN META -->
+        <!-- FIN META INFO -->
         <!-- CSS INCLUDE -->        
         <link rel="stylesheet" type="text/css" id="theme" href="css/theme-default.css"/>
         <!-- FIN CSS INCLUDE -->      
